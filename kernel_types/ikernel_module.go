ebbaaba7e5bfd326d22b34eda96f0f1669837362
SVI <robo.help@ya.ru>
2025-02-26 18:44:28 +0300

    SVI Добавление интерфейса модуля; 100.0%

---
+++ kernel_types/ikernel_module.go
@@ -0,0 +1,15 @@
+package kernel_types
+
+import (
+	. "github.com/prospero78/kern/kernel_alias"
+)
+
+// IKernelModule -- интерфейс к модулю на основе ядра
+type IKernelModule interface {
+	// Run -- запускает модуль в работу
+	Run()
+	// IsWork -- возвращает состояние модуля
+	IsWork() bool
+	// Name -- возвращает уникальное имя модуля
+	Name() AModuleName
+}
