ee544281b334f9b3fa88bc7c6d62aeca67aaabd1
SVI <robo.help@ya.ru>
2025-02-26 13:11:13 +0300

    SVI Добавление тестов; 90.1%

---
+++ mock/mock_hand_sub/mock_hand_serve_test.go
@@ -0,0 +1,64 @@
+package mock_hand_sub
+
+import (
+	"testing"
+)
+
+type tester struct {
+	t    *testing.T
+	hand *MockHandlerSub
+}
+
+func TestMockHandleServe(t *testing.T) {
+	sf := &tester{
+		t: t,
+	}
+	sf.new()
+	sf.back()
+}
+
+// Проверка обратного вызова
+func (sf *tester) back() {
+	sf.t.Log("back")
+	sf.backGood1()
+}
+
+func (sf *tester) backGood1() {
+	sf.t.Log("backGood1")
+	sf.hand.FnBack([]byte("test_msg"))
+	if string(sf.hand.Msg_) != "test_msg" {
+		sf.t.Fatalf("backGood1(): binMsg(%v)!='test_msg'", string(sf.hand.Msg_))
+	}
+}
+
+// Создание мок-обработчика запросов
+func (sf *tester) new() {
+	sf.t.Log("new")
+	sf.newBad1()
+	sf.newGood1()
+}
+
+func (sf *tester) newGood1() {
+	sf.t.Log("newGood1")
+	sf.hand = NewMockHandlerSub("test_topic", "test_name")
+	if sf.hand == nil {
+		sf.t.Fatalf("newGood1(): handler==nil")
+	}
+	if name := sf.hand.Name(); name != "test_name" {
+		sf.t.Fatalf("newGood1(): name(%v)!='test_name'", name)
+	}
+	if topic := sf.hand.Topic(); topic != "test_topic" {
+		sf.t.Fatalf("newGood1(): topic(%v)!='test_topic'", topic)
+	}
+}
+
+// Нет топика для создания
+func (sf *tester) newBad1() {
+	sf.t.Log("newBad1")
+	defer func() {
+		if _panic := recover(); _panic == nil {
+			sf.t.Fatalf("")
+		}
+	}()
+	_ = NewMockHandlerSub("", "test_name")
+}
