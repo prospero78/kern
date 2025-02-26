d6ae631ff3b3132f6ce4b6537a0812d954f8f5a7
SVI <robo.help@ya.ru>
2025-02-26 19:26:22 +0300

    SVI Добавление модуля; 100.0%

---
+++ kernel_types/ikernel_monolit.go
@@ -0,0 +1,13 @@
+package kernel_types
+
+// IKernelMonolit -- интерфейс к монолиту на основе ядра
+type IKernelMonolit interface {
+	// IsLocal -- возвращает признак локальной шины
+	IsLocal() bool
+	// IsWork -- возвращает признак работы монолита
+	IsWork() bool
+	// Run -- запускает монолит в работу
+	Run()
+	// Add -- добавляет модуль в монолит
+	Add(IKernelModule)
+}
